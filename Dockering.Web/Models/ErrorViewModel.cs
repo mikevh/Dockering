e758ce0ce40f7fa807ed2e5f4919ca6c65f98c98
Michael VanderHelm <mikevh@gmail.com>
2017-09-13 16:30:02 -0700

    dotnet new mvc

---
+++ Dockering.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dockering.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
